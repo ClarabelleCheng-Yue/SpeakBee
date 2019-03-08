467e964da770baa2b5d49aaf217c4b2e7f7cc4df
Clarabelle Cheng-Yue <cachengyue@gmail.com>
2019-03-07 17:48:25 -0800

    add training

--- code/getPattern.js
+++ code/getPattern.js
@@ -1,4 +1,6 @@
-module.exports.function = function getPattern (fromLang, toLang) {
+module.exports.function = function getPattern (fromLang, toLang, $vivContext) {
+  console.log('inside getPattern. args:', arguments);
+  
   return {
     from: fromLang || "EN",
     to: toLang
--- code/translate.js
+++ code/translate.js
@@ -1,7 +1,9 @@
 module.exports.function = function translate (fromTo, $vivContext) {
   console.log('fromto: ', fromTo);
-  console.log('viv context: ', $vivContext.utterance);
+  console.log('viv context utt: ', $vivContext.utterance);
   console.log('arguments: ', arguments);
+  const utt = $vivContext.utterance;
+  console.log('utt: ', utt);
   // TODO: add translator API here
   
   return "hi"
