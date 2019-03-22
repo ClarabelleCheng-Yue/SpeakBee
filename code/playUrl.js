0872ce9cfb55135b27131b396990768d41f34043
Clarabelle Cheng-Yue <cachengyue@gmail.com>
2019-03-21 21:28:43 -0700

    clean up code

--- code/playUrl.js
+++ code/playUrl.js
@@ -1,4 +1,3 @@
 module.exports.function = function playUrl (url) {
-  console.log('url:', url);
   return url;
 }
--- code/translate.js
+++ code/translate.js
@@ -4,17 +4,12 @@ const regex = /translate from (english|spanish) to (english|spanish|korean) (.*)
 const HI = "Hi ";
 
 module.exports.function = function translate ($vivContext) {
-  console.log('arguments: ', arguments);
-  console.log('viv context utt: ', $vivContext.utterance);
   const utt = $vivContext.utterance;
   const toXlate = (utt.indexOf(HI) > -1 && HI.length < utt.length) ? utt.substring(utt.indexOf(HI) + HI.length) : utt;
-  console.log('xlate: ', toXlate);
   var result = http.getUrl('https://xlate.herokuapp.com/xlate', {format: 'text', query:{toxlate: toXlate, format:'json', api:'naver'}});
   text = JSON.parse(result).translation;
-  console.log('url:' + 'https://xlate.herokuapp.com/xlate?toxlate='+ toXlate + '&format=json&api=naver' );
-  console.log('text: ', text);
   return {
     text: text || "o_o ?",
-    audioUrl: "http://www.google.com"
+    audioUrl: "" // Future feature: add audio translation
   }
 }
