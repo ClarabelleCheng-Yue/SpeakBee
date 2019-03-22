84a6b7194365d1bf46f76e88d320736ef29b1740
Clarabelle Cheng-Yue <cachengyue@gmail.com>
2019-03-21 21:08:18 -0700

    add training

--- code/translate.js
+++ code/translate.js
@@ -1,21 +1,20 @@
 var console = require ('console');
 var http = require('http');
 const regex = /translate from (english|spanish) to (english|spanish|korean) (.*)/i
-const HEY = "hey ";
+const HI = "Hi ";
 
 module.exports.function = function translate ($vivContext) {
   console.log('arguments: ', arguments);
   console.log('viv context utt: ', $vivContext.utterance);
   const utt = $vivContext.utterance;
-  // const parsed = regex.exec(utt);
-  const toXlate = utt.substring(utt.indexOf(HEY) > -1 ? utt.indexOf(HEY) + HEY.length : 0); // (parsed.length < 4) ? '' : parsed[3];
+  const toXlate = (utt.indexOf(HI) > -1 && HI.length < utt.length) ? utt.substring(utt.indexOf(HI) + HI.length) : utt;
   console.log('xlate: ', toXlate);
-  var result = http.getUrl('https://xlate.herokuapp.com/xlate', {format: 'text', query:{toxlate: toXlate, format:'json'}});
+  var result = http.getUrl('https://xlate.herokuapp.com/xlate', {format: 'text', query:{toxlate: toXlate, format:'json', api:'naver'}});
   text = JSON.parse(result).translation;
-  console.log('url:' + 'https://xlate.herokuapp.com/xlate?toxlate='+ toXlate + '&format=text' );
+  console.log('url:' + 'https://xlate.herokuapp.com/xlate?toxlate='+ toXlate + '&format=json&api=naver' );
   console.log('text: ', text);
   return {
-    text: text || "test",
+    text: text || "o_o ?",
     audioUrl: "http://www.google.com"
   }
 }
