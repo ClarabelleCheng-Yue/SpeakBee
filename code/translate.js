826a69b3de28c2e4c8293ec48cb5d1e064a46b9e
Clifton Craig <clifton.craig@gmail.com>
2019-03-24 20:45:58 -0700

    Inlines the Naver API call directly.

--- code/translate.js
+++ code/translate.js
@@ -4,11 +4,24 @@ const regex = /translate from (english|spanish) to (english|spanish|korean) (.*)
 // Wake word
 const HI = "Hi ";
 
-module.exports.function = function translate ($vivContext) {
+function xlateProxyAPI($vivContext) {
   const utt = $vivContext.utterance;
   const toXlate = (utt.indexOf(HI) > -1 && HI.length < utt.length) ? utt.substring(utt.indexOf(HI) + HI.length) : utt;
   var result = http.getUrl('https://xlate.herokuapp.com/xlate', {format: 'text', query:{toxlate: toXlate, format:'json', api:'naver'}});
   text = JSON.parse(result).translation;
+  return text;
+}
+
+module.exports.function = function translate ($vivContext) {
+  const utt = $vivContext.utterance;
+  const toXlate = (utt.indexOf(HI) > -1 && HI.length < utt.length) ? utt.substring(utt.indexOf(HI) + HI.length) : utt;
+  var options = {
+    headers: {'X-Naver-Client-Id': 'pdrAJgv_e215VWgXVRyN', 'X-Naver-Client-Secret': 'WnyHdHQ307'}, 
+    passAsJson: true,
+    format: 'json'
+  };
+  var result = http.postUrl('https://openapi.naver.com/v1/language/translate', {source: 'en', target: 'ko', text:toXlate}, options);
+  text = result.message.result.translatedText;
   return {
     text: text || "o_o ?"
     /* Future feature: add audio translation */
